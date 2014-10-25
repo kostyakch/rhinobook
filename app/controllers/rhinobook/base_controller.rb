fb223ad1cae60bf1e7bb6c9af40114c9f2a87e49
KCh <kch@list.ru>
2014-10-25 15:09:44 +0400

    base_controller throw rhinoart::base

--- app/controllers/rhinobook/base_controller.rb
+++ app/controllers/rhinobook/base_controller.rb
@@ -1,23 +1,7 @@
 require_dependency "rhinobook/application_controller"
 
 module Rhinobook
-	class BaseController < Rhinoart::ApplicationController
-		before_filter :check_if_user_has_admin_role
-		
-
-		rescue_from CanCan::AccessDenied do |exception|
-			if user_signed_in?
-				flash.now[:info] = "Access denied."
-				render :template => 'shared/no_approved', :status => 403
-			else
-				redirect_to new_user_session_path, alert: exception.message
-			end
-		end
-
-
-		private    
-			def check_if_user_has_admin_role
-				authorize! :access, :admin_panel
-			end		
+	class BaseController < Rhinoart::BaseController
+	
 	end
 end
