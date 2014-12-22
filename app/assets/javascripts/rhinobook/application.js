9eafc0c3b872e908dff16e522d7764817583a957
KCh <kch@list.ru>
2014-12-22 18:07:34 +0300

    Make adding many images and videos to page

--- app/assets/javascripts/rhinobook/application.js
+++ app/assets/javascripts/rhinobook/application.js
@@ -10,4 +10,29 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require_tree .
+// require_tree .
+
+$(function(){
+    
+	$(document).on("click", "[data-behavior*=add_new_image]",  function(e) {
+		e.preventDefault();
+		ids = [-1];
+
+		$(".image-list input[type=file]").each( function( index, element ) {			
+			ids.push(parseInt(element.name.match(/\[(\d+)\]/)[1], 10));
+		});				
+		newId = Math.max.apply(Math, ids) + 1;
+		$(".image-list").append($("[data-behavior*=add_new_image]").data("new").replace(/\[(\d+)\]/g, "[" + newId + "]"));
+	});
+
+	$(document).on("click", "[data-behavior*=add_new_video]",  function(e) {
+		e.preventDefault();
+		ids = [-1];
+
+		$(".video-list input[type=file]").each( function( index, element ) {			
+			ids.push(parseInt(element.name.match(/\[(\d+)\]/)[1], 10));
+		});				
+		newId = Math.max.apply(Math, ids) + 1;
+		$(".video-list").append($("[data-behavior*=add_new_video]").data("new").replace(/\[(\d+)\]/g, "[" + newId + "]"));
+	});
+})
